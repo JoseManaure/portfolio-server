2b3fa2763e97b96ececbf68a28a2a069226acd5c
Jose Manaure <jmanaure87@gmail.com>
2025-11-04 19:37:48 -0300

    modificando los tunels

--- utils/notifyN8n.js
+++ utils/notifyN8n.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 export async function notifyN8n(message, title = "Nuevo mensaje") {
     try {
-        const N8N_WEBHOOK = process.env.N8N_WEBHOOK_URL || "https://21064a753e80.ngrok-free.app/webhook/chat";
+        const N8N_WEBHOOK = process.env.N8N_WEBHOOK_URL || "https://f8e85894b3ed.ngrok-free.app/webhook/chat";
 
         // Enviar mensaje como JSON
         const res = await fetch(N8N_WEBHOOK, {
